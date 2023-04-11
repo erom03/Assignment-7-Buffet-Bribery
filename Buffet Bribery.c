6338a6e9c5bfac2d1baef8cea17c30632c6cb51e
Enrique Romero <enrique.romero1110@gmail.com>
2023-04-11 11:11:13 -0400

    Add file

---
+++ Buffet Bribery.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Shipment {
+    int arrival;
+    int expires;
+    int mass;
+} Shipment;
+
+int main() {
+    // Get input for number of shipments
+    int numShipments;
+    scanf("%d", &numShipments);
+
+    // Create heap to hold shipments
+
+    return 0;
+}
